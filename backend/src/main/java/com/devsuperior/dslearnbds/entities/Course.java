f397492df7a307e5843dffff2dc878353ee3c275
Rodrigo Acevedo <rodrigoacevedo@outlook.com>
2022-02-02 01:13:38 -0300

    Add entity Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_course")
@@ -17,6 +19,10 @@ public class Course  implements Serializable {
     private String imgUri;
 
     private String imgGrayUri;
+
+
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
 
     public Course() {
     }
@@ -58,6 +64,10 @@ public class Course  implements Serializable {
 
     public void setImgGrayUri(String imgGrayUri) {
         this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
